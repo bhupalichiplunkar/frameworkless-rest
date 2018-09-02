952a3f0d5854639192ca7478985958875b1333ad
bhupalichiplunkar <bhupali@greyatom.com>
2018-09-02 09:41:03 +0530

    - initial commit

---
+++ index.js
@@ -0,0 +1,101 @@
+/*
+  Primary file
+*/
+
+// dependencies
+const http = require("http");
+const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
+// the server should respond to all requests with a string
+
+const server = http.createServer((request, response) => {
+  // Get the URL and parse it
+  const parsedUrl = url.parse(request.url, true);
+  // Get path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  //Get query string an object
+  const queryObject = parsedUrl.query;
+  queryObject.toString = function() {
+    return JSON.stringify(this);
+  };
+  // get HTTP method
+  var method = request.method.toLowerCase();
+
+  //get headers
+  var headers = request.headers;
+  headers.toString = function() {
+    return JSON.stringify(this);
+  };
+
+  // get payload
+  var decoder = new StringDecoder("utf-8");
+  var buffer = "";
+  request.on("data", data => {
+    buffer += decoder.write(data);
+  });
+
+  request.on("end", () => {
+    buffer += decoder.end();
+
+    //choose handler which should process the request. if non are found send to not found handler
+
+    var chosenHandler = router[trimmedPath]
+      ? router[trimmedPath]
+      : handlers.notFound;
+
+    var data = {
+      trimmedPath,
+      queryObject,
+      method,
+      headers,
+      payload: buffer
+    };
+
+    chosenHandler(data, (statusCode, payload) => {
+      //default statusCode and payload to 200 and {} respectively
+      statusCode = typeof statusCode === "number" ? statusCode : 200;
+      payload = typeof payload === "object" ? payload : {};
+
+      var payloadString = JSON.stringify(payload);
+
+      // Send the response
+      response.writeHead(statusCode);
+      response.end(payloadString);
+      //Log request path
+      console.log(
+        `Request recieved at path : ${trimmedPath} 
+         method: ${method} 
+         query : ${queryObject} 
+         headers : ${headers}
+         payload: ${buffer}
+         payloadString: ${payloadString}`
+      );
+    });
+  });
+});
+
+// start the server and make it listen to port 5001
+server.listen(5001, () => {
+  console.log("Server is listening on port 5001");
+});
+
+//routing handlers
+
+var handlers = {};
+
+handlers.sample = (data, callback) => {
+  //perform business logic and return http status code and response data / payoad object
+  callback(406, { name: "Bhupali" });
+};
+
+//for 404
+handlers.notFound = (data, callback) => {
+  //perform business logic and return status code and response data
+  callback(404);
+};
+
+var router = {
+  sample: handlers.sample
+};
